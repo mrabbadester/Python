f32dd75b2344ff33ee3b51335857011dd6073893
mrabbadester <antos2lite@gmail.com>
2023-06-20 18:35:16 -0300

    primeiro commit

---
+++ transformeseSerasaPython/strings.py
@@ -0,0 +1,9 @@
+frase  = "Olá, mundo!"
+
+print("String original: ")
+print(frase);
+print();
+
+print("Acessando caracteres individuais: ");
+print("Primeiro caractere: ", frase[0]); 
+print("Último caractere: ", frase[-1]); 
---
+++ transformeseSerasaPython/variavel.py
@@ -0,0 +1,13 @@
+#/ treinamento /
+ 
+s = "banana";
+ss = "banana";
+
+print(s, id(s));
+
+print(ss);
+
+s.replace("b", "z");
+
+print(s);
+
