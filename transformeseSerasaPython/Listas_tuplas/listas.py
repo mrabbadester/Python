71c315e0c6035b171d29cb11ef7300960a2c2f3a
mrabbadester <antos2lite@gmail.com>
2023-06-22 21:02:21 -0300

    terceiro

---
+++ transformeseSerasaPython/Listas_tuplas/listas.py
@@ -0,0 +1,46 @@
+listaNumeros = [1,2,3,4,5];
+listaStrings = ['e', 'f', 'c', 'd'];
+listaMista = [1, 'a', 3.14, True]
+print(listaNumeros);
+print(listaStrings);
+print(listaMista);
+print();
+
+frutas = ["maça", "banana", "morango"];
+
+print(frutas[0]);
+print();
+
+frutas[1] = "laranja";
+print("Tamanho 1: ", len(frutas));
+frutas.append("abacaxi");
+print(frutas);
+
+print("tamanho 2: ", len(frutas));
+print();
+
+listaNova = [1, ["a", "b"]];
+print(listaNova[1][0]);
+print();
+
+lista1 = [1,2,3];
+lista2 = [4,5,6];
+
+lista_concatenada = lista1 + lista2;
+print(lista_concatenada);
+print();
+
+listaRepetida = [0] * 3;
+print(listaRepetida);
+print();
+
+numeros = [1,2,3,4];
+sublista = numeros[1:4];
+print(sublista);
+print();
+
+numeros = ["a","b","c","d"];
+sublista = numeros[1:4];
+print(sublista);
+print();
+
