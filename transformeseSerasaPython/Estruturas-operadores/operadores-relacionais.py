1e47cbc90ef61c3502c28a1b1df4649eb101641c
mrabbadester <antos2lite@gmail.com>
2023-06-29 20:26:12 -0300

    operadores atualização

---
+++ transformeseSerasaPython/Estruturas-operadores/operadores-relacionais.py
@@ -0,0 +1,28 @@
+x = 5;
+y = 7;
+
+#maior que 
+print(x > y); #false
+print();
+
+#menor o que  
+print(x < y); #true
+print()
+
+#igualidade
+print(x == y); #false
+print();
+
+#maior ou igual
+print(x >= y); #false
+print();
+
+#menos ou igual
+print(x <= y); #true
+print();
+
+#diferente de 
+print(x != y); #true
+print();
+
+#
